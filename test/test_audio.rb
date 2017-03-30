3448b0b6bd159a76f92b746bf7c36c14cc6c6a3b
Kjarrigan <github@kjarrigan.de>
2017-03-30 22:10:37 +0200

    Improve AudioTests

--- test/test_audio.rb
+++ test/test_audio.rb
@@ -6,16 +6,109 @@ class TestAudio < Minitest::Test
   include TestHelper
   include InteractiveTests
   
-  def test_audio
-    audio = Gosu::Audio.new(media_path('0614.ogg'))
+  def test_sample
+    sound = Gosu::Sample.new(media_path('0614.ogg'))
     
-    stream = audio.play
-    assert stream.playing?
-    
+    channel = sound.play(1,1,true)
     interactive_cli("Do you hear a StarWars like Blaster sound?")
     
-    stream.stop
-    refute stream.playing?    
+    channel.volume = 0.5
+    interactive_cli("Is the volume lowered?")
+    channel.volume = 1.0
     
-  end  
+    channel.speed = 2.0
+    interactive_cli("Does it play faster?")
+    
+    channel.pan = -1.0
+    interactive_cli("Only left speaker?")
+    
+    channel.pan =  1.0
+    interactive_cli("Only right speaker?")
+    
+    channel.pan =  0.0
+    interactive_cli("Both speaker again?")
+    
+    refute channel.paused?
+    assert channel.playing?
+    
+    channel.pause
+    interactive_cli("Did it stop?")
+    
+    assert channel.paused?
+    refute channel.playing?
+
+    channel.resume
+    interactive_cli("Back again?")
+    
+    refute channel.paused?
+    assert channel.playing?    
+    
+    channel.stop
+    
+    refute channel.paused?
+    refute channel.playing?
+    
+    channel = sound.play_pan(1,1,0.5,false)
+    interactive_cli("Right speaker again but slowmotion this time?")
+    
+  ensure
+    channel.stop
+  end
+  
+  class SongTestWindow < Gosu::Window
+    def initialize
+      super(50,50)
+      self.caption = "Song-Test"
+    end
+  end
+  
+  def test_song
+    # At the moment we need a window to hear more than one "tick" of a song.
+    win = SongTestWindow.new
+    
+    assert_nil Gosu::Song.current_song
+    
+    song = Gosu::Song.new(media_path('0830.ogg'))
+
+    song.play(true)
+    interactive_cli("Do you hear a churchbell?") do
+      200.times do win.tick end
+    end
+    
+    song.volume = 0.5
+    interactive_cli("Is the volume lowered?") do
+      200.times do win.tick end
+    end
+    song.volume = 1.0
+    
+    assert_equal song, Gosu::Song.current_song
+    
+    refute song.paused?
+    assert song.playing?
+    
+    song.pause
+    interactive_cli("Did it stop?") do
+      200.times do win.tick end
+    end
+    
+    assert_equal song, Gosu::Song.current_song
+    
+    assert song.paused?
+    refute song.playing?
+
+    song.resume
+    interactive_cli("Back again?") do
+      200.times do win.tick end
+    end
+    
+    refute song.paused?
+    assert song.playing?    
+    
+    song.stop
+    
+    refute song.paused?
+    refute song.playing?    
+    
+    assert_nil Gosu::Song.current_song
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,9 +12,15 @@ module InteractiveTests
   def interactive_cli(message)
     return false unless ENV["GOSU_TEST_INTERACTIVE"]
     
-    STDOUT.puts message + 'Type (Y)es or (N)o and ENTER'
+    STDOUT.puts message + 'Type (Y)es or (N)o  or (S)kip and ENTER'
     yield if block_given?
-    assert STDIN.gets.chomp =~ /[yY]/, "User answered 'No' to '#{message}'"
+    
+    user_input = STDIN.gets
+    if user_input =~ /[sS]/
+      skip
+    else
+      assert user_input =~ /[yY]/, "User answered 'No' to '#{message}'"
+    end
   end
   
   def interactive_gui(message)
